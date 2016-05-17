0ef9b8ae85606fd4647cd57669bcfdea33476790
Alpar Gabos <alpar.gabos@prezi.com>
2016-05-17 02:07:32 +0200

    animation utils, circular exit animation

--- app/src/main/java/com/alpargabos/imda/utils/AnimatorUtils.java
+++ app/src/main/java/com/alpargabos/imda/utils/AnimatorUtils.java
@@ -1,20 +1,28 @@
 package com.alpargabos.imda.utils;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
 import java.util.List;
 
 public class AnimatorUtils {
+
+	public static final int CIRCULAR_DURATION = 600;
+	public static final int ANIM_DURATION = 400;
+
 	@NonNull
-	public Animation createAnimation(Context context, int animatioResource, final List<View> views) {
-		Animation fadeOut = AnimationUtils.loadAnimation(context, animatioResource);
-		fadeOut.setStartTime(AnimationUtils.currentAnimationTimeMillis());
-		fadeOut.setDuration(400);
-		fadeOut.setAnimationListener(new Animation.AnimationListener() {
+	public Animation createAnimation(Context context, final int animatioResource, final List<View> views) {
+		Animation animation = AnimationUtils.loadAnimation(context, animatioResource);
+		animation.setStartTime(AnimationUtils.currentAnimationTimeMillis());
+		animation.setDuration(ANIM_DURATION);
+		animation.setAnimationListener(new Animation.AnimationListener() {
 			@Override
 			public void onAnimationStart(Animation animation) {
 
@@ -23,7 +31,7 @@ public class AnimatorUtils {
 			@Override
 			public void onAnimationEnd(Animation animation) {
 				for (View view : views) {
-					view.setVisibility(View.GONE);
+					view.setVisibility(animatioResource == android.R.anim.fade_out ? View.GONE : View.VISIBLE);
 				}
 			}
 
@@ -33,8 +41,40 @@ public class AnimatorUtils {
 			}
 		});
 		for (View view : views) {
-			view.setAnimation(fadeOut);
+			view.setAnimation(animation);
 		}
-		return fadeOut;
+		return animation;
+	}
+
+	@NonNull
+	public Animator createCircularReveal(@NonNull View layout, int startX, int startY) {
+		// get the final radius for the clipping circle
+		int finalRadius = Math.max(layout.getWidth(), layout.getHeight());
+		// create the animator for this view (the start radius is zero)
+		Animator anim = ViewAnimationUtils.createCircularReveal(layout, startX, startY, 0, finalRadius);
+		// make the view visible and start the animation
+		anim.setInterpolator(new AccelerateInterpolator());
+		anim.setDuration(CIRCULAR_DURATION);
+		layout.setVisibility(View.VISIBLE);
+		return anim;
+	}
+
+	@NonNull
+	public Animator createExitReveal(@NonNull final View layout, int endX, int endY) {
+		// get the final radius for the clipping circle
+		int finalRadius = Math.max(layout.getWidth(), layout.getHeight());
+		// create the animator for this view (the start radius is zero)
+		Animator anim = ViewAnimationUtils.createCircularReveal(layout, endX, endY, finalRadius, 0);
+		// make the view visible and start the animation
+		anim.setInterpolator(new AccelerateInterpolator());
+		anim.setDuration(CIRCULAR_DURATION);
+		anim.addListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				super.onAnimationEnd(animation);
+				layout.setVisibility(View.INVISIBLE);
+			}
+		});
+		return anim;
 	}
 }
--- app/src/main/java/com/alpargabos/imda/welcome/WelcomeActivity.java
+++ app/src/main/java/com/alpargabos/imda/welcome/WelcomeActivity.java
@@ -2,21 +2,73 @@ package com.alpargabos.imda.welcome;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnTouch;
 import com.alpargabos.imda.R;
-import com.alpargabos.imda.utils.ActivityUtils;
+import com.alpargabos.imda.utils.AnimatorUtils;
+
+import java.util.Arrays;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+	@BindView(R.id.welcome_login_button) Button loginButton;
+	@BindView(R.id.welcome_signup_button) Button signupButton;
+	@BindView(R.id.welcome_texts) View textsContainer;
+	@BindView(R.id.login_layout) View loginLayout;
+	@BindView(R.id.signup_layout) View signupLayout;
+	@BindView(R.id.welcome_container) View welcomeContainer;
+
+	AnimatorUtils animatorUtils = new AnimatorUtils();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+		ButterKnife.bind(this);
+	}
 
-		WelcomeFragment fragment = (WelcomeFragment) getSupportFragmentManager().findFragmentById(R.id.welcome_container);
-		if (fragment == null) {
-			// Create the fragment
-			fragment = WelcomeFragment.newInstance();
-			ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), fragment, R.id.welcome_container);
+	@Override
+	public void onBackPressed() {
+		if (loginLayout.getVisibility() == View.VISIBLE) {
+			animatorUtils.createAnimation(this, android.R.anim.fade_in, Arrays.asList(textsContainer, loginButton, signupButton)).start();
+			int endX = loginButton.getLeft() + loginButton.getWidth() / 2;
+			int endY = (loginButton.getTop() - (welcomeContainer.getHeight() - loginLayout.getHeight())) + loginButton.getHeight() / 2;
+			animatorUtils.createExitReveal(loginLayout, endX, endY).start();
+		} else if (signupLayout.getVisibility() == View.VISIBLE) {
+			animatorUtils.createAnimation(this, android.R.anim.fade_in, Arrays.asList(textsContainer, loginButton, signupButton)).start();
+			int endX = signupButton.getLeft() + signupButton.getWidth() / 2;
+			int endY = (signupButton.getTop() - (welcomeContainer.getHeight() - signupLayout.getHeight())) + signupButton.getHeight() / 2;
+			animatorUtils.createExitReveal(signupLayout, endX, endY).start();
+		} else {
+			super.onBackPressed();
 		}
+	}
+
+	@OnTouch(R.id.welcome_login_button)
+	public boolean onTouchLogin(View view, MotionEvent motionEvent) {
+		if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+			animatorUtils.createAnimation(this, android.R.anim.fade_out, Arrays.asList(textsContainer, loginButton, signupButton)).start();
+
+			int startX = (int) (loginButton.getLeft() + motionEvent.getX());
+			int startY = (int) (motionEvent.getRawY() - (welcomeContainer.getHeight() - loginLayout.getHeight())) - view.getHeight() / 2;
+			animatorUtils.createCircularReveal(loginLayout, startX, startY).start();
+		}
+		return false;
+	}
+
+	@OnTouch(R.id.welcome_signup_button)
+	public boolean onTouchSignup(View view, MotionEvent motionEvent) {
+		if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+			animatorUtils.createAnimation(this, android.R.anim.fade_out, Arrays.asList(textsContainer, loginButton, signupButton)).start();
+
+			int startX = (int) (signupButton.getLeft() + motionEvent.getX());
+			int startY = (int) (motionEvent.getRawY() - (welcomeContainer.getHeight() - signupLayout.getHeight())) - view.getHeight() / 2;
+			animatorUtils.createCircularReveal(signupLayout, startX, startY).start();
+		}
+		return false;
 	}
 }
